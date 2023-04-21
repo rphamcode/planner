40c1c0296076fb3df8e7aa379c0aa79185d12951
Raul Pham <phatpham314@icloud.com>
2023-04-21 13:15:44 -0500

    Initial Commit

---
+++ planner/plannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  plannerApp.swift
+//  planner
+//
+//  Created by Pham on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct plannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
